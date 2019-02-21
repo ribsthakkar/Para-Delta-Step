cbd50d7d8b6f498506d4f9a801dd717e9b1535e9
ribsthakkar <rishabh.thakkar@gmail.com>
2019-02-21 10:57:20 -0600

    Initial Commit
    Includes an existing user's sequential Delta Stepping algorithm integrated with mini graph implementation created for Competitive programming class

---
+++ Graph.py
@@ -0,0 +1,107 @@
+import heapq
+
+
+class Graph():
+    def __init__(self, nodes):
+        self.num = nodes
+        self._nodes = list(Node(x) for x in range(self.num + 1))
+        self._edges = dict()
+
+    def add_edge(self, start, end, weight):
+        self._nodes[start].add_edge(self._nodes[end], weight)
+        if (start, end) in self._edges:
+            d = self._edges[(start, end)]
+            if d['weight'] < weight:
+                d['weight'] = weight
+        else:
+            self._edges[(start, end)] = {'weight': weight}
+
+
+    def get_edge_data(self, u, v):
+        return self._edges[(u,v)]
+
+    def unmark_all(self):
+        for node in self._nodes:
+            node.unmark()
+
+    def nodes(self):
+        return list(node.id for node in self._nodes)
+
+    def neighbors(self, vert):
+        return list(node .id for node in self._nodes[vert].adjacent)
+
+    def dijkstra(self, start):
+        q = []
+        q.append(self._nodes[start])
+        self._nodes[start].weight = 0
+        heapq.heapify(q)
+        while len(q):
+            current = heapq.heappop(q)
+            if current.visit:
+                continue
+            current.mark()
+            for adj, edge_weight in current.adjacent.items():
+                if not adj.visit:
+                    if adj.weight > current.weight + edge_weight:
+                        adj.weight = current.weight + edge_weight
+                        adj.prev = current
+                    heapq.heappush(q, adj)
+        self.unmark_all()
+        d = dict()
+        for i in range(1, len(self._nodes)):
+            d[i] = self._nodes[i].weight
+        return d
+
+    def pathfromHere(self, current):
+        path = [current]
+        while current.prev:
+            path.append(current.prev)
+            current = current.prev
+        return reversed(path)
+
+
+class Node():
+    def __init__(self,x):
+        self.id = x
+        self.visit = False
+        self.adjacent = {}
+        self.weight = float('inf')
+        self.prev = None
+
+    # Updating adjacent edge if we have a more minimum cost one
+    def add_edge(self, other, weight):
+        if other not in self.adjacent:
+            self.adjacent[other] = weight
+        elif self.adjacent[other] > weight:
+            self.adjacent[other] = weight
+
+    # Node comparator functions for HeapQ
+    def __lt__(self, other):
+        return self.weight < other.weight
+
+    def __gt__(self, other):
+        return self.weight > other.weight
+
+    # def __eq__(self, other):
+    #     return self.weight == other.weight
+
+    def __le__(self, other):
+        return self.weight <= other.weight
+
+    def __ge__(self, other):
+        return self.weight >= other.weight
+
+    def __ne__(self, other):
+        return self.weight != other.weight
+
+    def __hash__(self):
+        return self.id
+
+    def mark(self):
+        self.visit = True
+
+    def unmark(self):
+        self.visit = False
+
+    def __str__(self):
+        return str(self.id)
---
+++ deltaStep.py
@@ -0,0 +1,190 @@
+"""
+This code is an implementation of Prateek Srivastava's sequential delta stepping algorithm with a personal Graph class implementation.
+The goal is to make it parallelized. Prateek's impelementation is sequential.
+Prateek's code can be found at: https://github.com/prateek22sri/Graph-Delta-Stepping-SSSP/blob/master/deltaStepping.py
+
+"""
+
+from math import floor, sqrt
+import Graph as gr
+import matplotlib.pyplot as plt
+from pprint import pprint
+
+
+class Algorithm:
+    """
+    """
+
+    def __init__(self):
+        """
+        """
+        self.distances = {}
+        self.delta = 5
+        self.property_map = {}
+        self.workItems = []
+        self.source_vertex = 2
+        self.infinity = float("inf")
+        self.totalNodes = 0
+        self.totalEdges = 0
+        self.B = {}
+
+    def relax(self, w, x):
+        """
+        This function relaxes a bucket i.e. if the distance of a vertex is less than the already existing distance in
+        the property map then, the vertex is removed from the bucket and reinserted in the new bucket
+        x is the distance of the vertex and w is the index of the vertex in the property map
+        """
+        # print("w=", w, "x=", x)
+        if x < self.property_map[w]:
+            # check if there is an entry of w in the dictionary B
+            if self.property_map[w] != self.infinity:
+                if w in self.B[floor(self.property_map[w] / self.delta)]:
+                    # check if the vertex is in the wrong bucket
+                    if floor(x / self.delta) != floor(self.property_map[w] / self.delta):
+                        self.B[floor(self.property_map[w] / self.delta)].remove(w)
+                self.B[floor(x / self.delta)].append(w)
+
+            # if the dictionary entry does not exist
+            else:
+                if floor(x / self.delta) not in self.B:
+                    self.B[floor(x / self.delta)] = [w]
+                else:
+                    if w not in self.B[floor(x / self.delta)]:
+                        self.B[floor(x / self.delta)].append(w)
+
+            # update the property map
+            self.property_map[w] = x
+
+    def find_requests(self, vertices, kind, g):
+        """
+        returns a dictionary of neighboring edges with their weights but according to the kind i.e. light or heavy
+        :param vertices:
+        :param kind:
+        :param g:
+        :return:
+        """
+
+        tmp = {}
+        # print("vertices=", vertices, "kind=", kind)
+        for u in vertices:
+            for v in g.neighbors(u):
+                # print(u, self.property_map[u], g.get_edge_data(u, v)['weight'])
+                edge_weight = self.property_map[u] + g.get_edge_data(u, v)['weight']
+                if kind == 'light':
+                    if g.get_edge_data(u, v)['weight'] <= self.delta:
+                        if v in tmp:
+                            if edge_weight < tmp[v]:
+                                tmp[v] = edge_weight
+                        else:
+                            tmp[v] = edge_weight
+                elif kind == 'heavy':
+                    if g.get_edge_data(u, v)['weight'] > self.delta:
+                        if v in tmp:
+                            if edge_weight < tmp[v]:
+                                tmp[v] = edge_weight
+                        else:
+                            tmp[v] = edge_weight
+                else:
+                    return "Error: No such kind of edges " + kind
+        # print("tmp=", tmp)
+        return tmp
+
+    def relax_requests(self, request):
+        """
+        :param request:
+        :return:
+        """
+        for key, value in request.items():
+            self.relax(key, value)
+
+    def delta_stepping(self, g):
+        """
+        This is the main function to implement the algorithm
+        :param g:
+        :return:
+        """
+        for node in g.nodes():
+            self.property_map[node] = self.infinity
+        # print(self.B, self.property_map)
+        self.relax(self.source_vertex, 0)
+        # print(self.B, self.property_map)
+        ctr = 0
+        while self.B:
+            # print("Parent Iteration=", ctr)
+            # print("bucket=", self.B)
+            i = min(self.B.keys())
+            sub_ctr = 0
+            r = []
+
+            while i in self.B:
+                # print("Child Iteration=", sub_ctr)
+                # print("B[i]=", self.B[i])
+                req = self.find_requests(self.B[i], 'light', g)
+                # print("req=", req)
+                r += self.B[i]
+                del self.B[i]
+                self.relax_requests(req)
+                sub_ctr += 1
+                # print(self.B)
+            # print("child ends")
+            # print("r=", r)
+            req = self.find_requests(r, 'heavy', g)
+            self.relax_requests(req)
+            ctr += 1
+
+    def validate(self, g):
+        """
+        :param g:
+        :return:
+        """
+        self.property_map = {k: v for k, v in self.property_map.items() if v != self.infinity}
+        p = {k: v for k, v in g.dijkstra(self.source_vertex).items() if v != self.infinity}
+        if p == self.property_map:
+            return True
+        else:
+            print("Error: The algorithm is faulty!!!")
+            for i in range(1, len(p[0])):
+                if p[i] != self.property_map[i]:
+                    print("vertex ", i, " value in ground truth is ", p[i], " and value in delta stepping is ",
+                          self.property_map[i])
+            return False
+
+
+def main():
+    # make_graph = False
+    # g = nx.read_edgelist('sample2', nodetype=int, data=(('weight', int),), create_using=nx.DiGraph())
+
+    # print("\nGraph Information..")
+    # print("===================\n")
+    # print(nx.info(g))
+    # print("\nCalculating shortest path..")
+    details = list(map(int, input().split()))
+    num_nodes = details[0]
+    num_edges = details[1]
+    g = gr.Graph(num_nodes)
+    for edge in range(num_edges):
+        e = list(map(int, input().split()))
+        g.add_edge(e[0], e[1], e[2])
+    a = Algorithm()
+    a.source_vertex = details[2]
+    a.delta_stepping(g)
+    print("\nValidating Solution..")
+    if not a.validate(g):
+        exit(1)
+    else:
+        print("\nThe shortest path from ", a.source_vertex, " is:")
+        pprint(a.property_map)
+
+    # # visualize the graph
+    # if make_graph:
+    #     pos = nx.spring_layout(g, k=5 / sqrt(g.order()))
+    #     nx.draw_networkx(g, pos)
+    #     edge_labels = dict([((u, v,), d['weight'])
+    #                         for u, v, d in g.edges(data=True)])
+    #     nx.draw_networkx_edge_labels(g, pos=pos, edge_labels=edge_labels, label_pos=0.3, font_size=7)
+    #     plt.show(block=False)
+    #     plt.savefig("sample1_graph.png")
+
+
+if __name__ == '__main__':
+    main()
