aa423d06cf47c811e844cb709dca0815216274ef
ribsthakkar <rishabh.thakkar@gmail.com>
2019-02-25 12:06:56 -0600

    Finished adding rest of Delta Implementation, but there is still logic error

--- JavaDelta/src/Delta.java
+++ JavaDelta/src/Delta.java
@@ -1,10 +1,13 @@
+import com.sun.javaws.exceptions.InvalidArgumentException;
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
 import java.util.*;
 
 public class Delta {
 	private int delta, source, totalNodes, totalEdges;
 	private HashMap<Integer, Integer> distances;
 	private HashMap<Integer, ArrayList<Integer>> bucket;
-	private HashMap<Integer, Integer> property_map;
+	public HashMap<Integer, Integer> property_map;
 
 	public Delta(int d, int source, int tn, int te) {
 		this.source = source;
@@ -49,6 +52,71 @@ public class Delta {
 			}
 		}
 	}
-	
+
+	public HashMap<Integer, Integer> find_requests(ArrayList<Integer> vertices, String kind, Graph<Integer> g) {
+		HashMap<Integer, Integer> output = new HashMap<>();
+		for(int u: vertices) {
+			for(int v: g.getAdjacentVertices(u)) {
+				int edgeWeight = property_map.get(u) + g.getEdgeWeight(u, v);
+				if (kind.equals("light")) {
+					if(g.getEdgeWeight(u,v) <= delta) {
+						if(output.containsKey(v)) {
+							if(edgeWeight < output.get(v)) {
+								output.put(v, edgeWeight);
+							}
+						} else {
+							output.put(v, edgeWeight);
+						}
+					}
+				} else if (kind.equals("heavy")) {
+					if(g.getEdgeWeight(u,v) > delta) {
+						if(output.containsKey(v)) {
+							if(edgeWeight < output.get(v)) {
+								output.put(v, edgeWeight);
+							}
+						} else {
+							output.put(v, edgeWeight);
+						}
+					}
+				} else {
+					System.err.println("Invalid edge type");
+					System.exit(1);
+				}
+			}
+		}
+		return output;
+	}
+
+	public void relax_requests(HashMap<Integer,Integer> requests) {
+		for (Integer key : requests.keySet()) {
+			int value = requests.get(key);
+			relax(key, value);
+		}
+	}
+
+	public void delta_stepping(Graph<Integer> g) {
+		for(int node: g.getVertexList()) {
+			property_map.put(node, Integer.MAX_VALUE);
+		}
+
+		relax(source, 0);
+		HashMap<Integer, Integer> req;
+		int ctr = 0;
+		while(bucket.size() > 0) {
+			int i = Collections.min(bucket.keySet());
+			int sub_ctr = 0;
+			ArrayList<Integer> r = new ArrayList<>();
+			while(bucket.containsKey(i)) {
+				req = find_requests(bucket.get(i), "light", g);
+				r.addAll(bucket.get(i));
+				bucket.remove(i);
+				relax_requests(req);
+				sub_ctr +=1;
+			}
+			req = find_requests(r, "heavy", g);
+			relax_requests(req);
+			ctr += 1;
+		}
+	}
 	
 }
