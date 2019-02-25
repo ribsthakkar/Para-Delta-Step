a61df67861e6accc57137b1aa4773b4370a9c515
ribsthakkar <rishabh.thakkar@gmail.com>
2019-02-25 15:35:54 -0600

    Changed Graph Class and Implemented Sequential Delta Step
    Still in the middle of implementing the parallelized

--- JavaDelta/src/Main.java
+++ JavaDelta/src/Main.java
@@ -1,88 +1,69 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.PriorityQueue;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 
 		System.out.println("Hello World!");
 
-		Graph<Integer> graph = new Graph<>(true);
-		ArrayList<Edge<Integer>> connected = new ArrayList<>();
-		connected.add(new Edge<>(2, 2));
-		connected.add(new Edge<>(3, 12));
-		connected.add(new Edge<>(4, 7));
-		graph.add(1, connected);
+		Graph graph = new Graph(true, 7);
 
+		graph.addArc(1, 2, 2);
+		graph.addArc(1, 3, 12);
+		graph.addArc(1, 4, 7);
 		graph.addArc(3, 2, 1);
 
-		graph.addArc(8, 6, 4);
+		graph.addArc(5, 6, 4);
 
-		graph.addArc(8, 7, 5);
+		graph.addArc(5, 7, 5);
 
-		graph.addArc(4, 8, 1);
+		graph.addArc(4, 5, 1);
 
 		graph.addArc(6, 4, 1);
 
-		graph.addArc(8, 1, 1);
+		graph.addArc(5, 1, 1);
 
 		System.out.println(graph.toString());
 
-		System.out.println(Main.dijkstraShortestPath(graph, 8));
-		Delta d = new Delta(5, 8, graph.numNodes, graph.numEdges);
-		d.delta_stepping(graph);
-		for(int node:d.property_map.keySet()) {
-			System.out.println("Cost to Node " + node + " is "+ d.property_map.get(node));
-		}
-	}
+		Delta d = new Delta(5, 5, );
+		long totalTime = 0;
+		long dTotalTime = 0;
+		long startTime;
+		long endTime;
+		for(int i = 0; i < 10000000; i ++) {
+			startTime = System.nanoTime();
+			d.delta_stepping(graph);
+			endTime = System.nanoTime();
+			totalTime += endTime - startTime;
 
-	public static <V> HashMap<V, Double> dijkstraShortestPath(Graph<V> graph, V source) {
-		HashMap<V, Double> distances = new HashMap<V, Double>();
-		ArrayList<V> queue = new ArrayList<V>();
-		ArrayList<V> visited = new ArrayList<V>();
-		queue.add(0, source);
-		distances.put(source, 0.0);
-		while (!queue.isEmpty()) {
-
-			V currentVertex = queue.remove(queue.size() - 1);
-
-			// to save time we initialize all the distances to infinity as we go
-			distances.putIfAbsent(currentVertex, Double.POSITIVE_INFINITY);
-			for (V adjacentVertex : graph.getAdjacentVertices(currentVertex)) {
-				distances.putIfAbsent(adjacentVertex, Double.POSITIVE_INFINITY);
-
-				// if the distance between the source and the adjacent vertex is
-				// greater than the distance between the source and the current
-				// vertex PLUS the weight between the current and adjacent
-				// vertex, then we have found a shorter path than already
-				// existed
-				if (true) {
-
-					if (distances.get(adjacentVertex) > graph.getDistanceBetween(currentVertex, adjacentVertex)
-							+ distances.get(currentVertex)) {
-
-						distances.put(adjacentVertex,
-								graph.getDistanceBetween(currentVertex, adjacentVertex) + distances.get(currentVertex));
-					}
-				}
-
-				if (!visited.contains(adjacentVertex) && !queue.contains(adjacentVertex)) {
-					queue.add(0, adjacentVertex);
+			startTime = System.nanoTime();
+			HashMap<Integer, Integer> dij = graph.dijsktra(5);
+			endTime = System.nanoTime();
+			dTotalTime += endTime - startTime;
+			for(int node:d.property_map.keySet()) {
+				int out = dij.get(node);
+				int other = d.property_map.get(node);
+				if(out != other) {
+					System.out.println("Incorrect execution");
+					System.out.println("Dijkstra gave " + out);
+					System.out.println("Delta gave " + other);
 				}
 			}
-			visited.add(currentVertex);
 
 		}
-
-		// since the above statments only added the vertices as needed,
-		// verticies that are completely unconnected to the source are not added
-		// yet, so this adds them now
-		for (V v : graph.getVertexList()) {
-			if (!distances.containsKey(v)) {
-				distances.put(v, Double.POSITIVE_INFINITY);
-			}
-		}
-
-		return distances;
+		long time = (totalTime) / 10000000;
+		long dTime = (dTotalTime) / 10000000;
+		System.out.printf("Delta takes Average %d nanoseconds\n", time);
+		System.out.printf("Dijkstra takes Average %d nanoseconds\n", dTime);
+//
+//		System.out.println(graph.dijsktra(5));
+//		d.delta_stepping(graph);
+//
+//		for(int node:d.property_map.keySet()) {
+//			System.out.println("Cost to Node " + node + " is "+ d.property_map.get(node));
+//		}
 	}
 }
--- JavaDelta/src/Node.java
+++ JavaDelta/src/Node.java
@@ -1,13 +1,61 @@
+import org.omg.PortableInterceptor.INACTIVE;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Node implements Comparable<Node> {
 
     private int weight;
     private int id;
     private boolean visit;
+    private HashMap<Node, Integer> adjacent;
 
+    public Node(int id) {
+        this.id = id;
+        visit = false;
+        adjacent = new HashMap<>();
+        weight = Integer.MAX_VALUE;
+    }
+
+    public void addEdge(Node other, int weight) {
+        if(!adjacent.containsKey(other)) {
+            adjacent.put(other, weight);
+        } else if(adjacent.get(other) < weight) {
+            adjacent.put(other, weight);
+        }
+    }
+    public void mark() {
+        visit = true;
+    }
+    public void unmark() {
+        visit = false;
+    }
+
+    public HashMap<Node, Integer> getAdjacent() {
+        return adjacent;
+    }
+    public synchronized void setWeight(int weight) {
+        this.weight = weight;
+    }
+    public boolean isVisited() {
+        return visit;
+    }
+    public synchronized int getWeight() {
+        return weight;
+    }
+    public int getID() {
+        return id;
+    }
     @Override
     public int compareTo(Node o) {
         return this.weight - o.weight;
     }
+
+    @Override
+    public String toString() {
+        return "" + id;
+    }
+
 }
